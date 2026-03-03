1610a143d3fa3d66f46e05694e7b78f0048673aa
Cahyadi-Prasetyo <cahyadiprasetyo659@gmail.com>
2026-03-03 11:21:20 +0700

    fix: explicit dataset-level display:true on wisman charts to prevent label cutoff across overlapping numbers

--- assets/js/modal-batam.js
+++ assets/js/modal-batam.js
@@ -345,7 +345,7 @@ function openModal(type, regionKey = 'kepulauan_riau', regionName = 'Kepulauan R
                                 offset: 2,
                                 formatter: (v) => v.toLocaleString('id-ID'),
                                 color: '#1e293b',
-                                font: { weight: '600', size: 9 }
+                                font: { weight: '600', size: 9 }, display: true
                             }
                         },
                         {
