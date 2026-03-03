6462d16edb2a380e673570aa7906049359d39422
Cahyadi-Prasetyo <cahyadiprasetyo659@gmail.com>
2026-03-03 11:00:32 +0700

    feat: update 2026 data for inflasi and adjust modal labels for consistency

--- assets/data/inflasi.js
+++ assets/data/inflasi.js
@@ -8,28 +8,28 @@ const dataInflasi = {
       "nama": "Kota Batam",
       "tahunan": {
         "2025": [2.54, 2.88, 2.53, 2.81, 2.04, 1.68, 2.30, 2.37, 2.82, 3.19, 3.21, 3.68],
-        "2026": [2.74, null, null, null, null, null, null, null, null, null, null, null]
+        "2026": [2.74, 3.13, null, null, null, null, null, null, null, null, null, null]
       }
     },
     "tanjungpinang": {
       "nama": "Kota Tanjungpinang",
       "tahunan": {
         "2025": [-0.44, -0.97, 0.07, 1.11, 0.36, 0.07, 0.91, 1.23, 1.82, 2.15, 2.16, 2.75],
-        "2026": [4.32, null, null, null, null, null, null, null, null, null, null, null]
+        "2026": [4.32, 5.83, null, null, null, null, null, null, null, null, null, null]
       }
     },
     "karimun": {
       "nama": "Kabupaten Karimun",
       "tahunan": {
         "2025": [0.71, -0.73, 0.15, 2.30, 0.87, -0.15, 0.40, 1.92, 2.91, 2.58, 2.43, 2.72],
-        "2026": [2.77, null, null, null, null, null, null, null, null, null, null, null]
+        "2026": [2.77, 4.13, null, null, null, null, null, null, null, null, null, null]
       }
     },
     "kepulauan_riau": {
       "nama": "Provinsi Kepulauan Riau",
       "tahunan": {
         "2025": [2.01, 2.09, 2.01, 2.56, 1.73, 1.32, 1.97, 2.19, 2.70, 3.01, 3.00, 3.47],
-        "2026": [2.94, null, null, null, null, null, null, null, null, null, null, null]
+        "2026": [2.94, 3.54, null, null, null, null, null, null, null, null, null, null]
       }
     }
   }
